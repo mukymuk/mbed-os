293d0d808b6f422d45020c2b9df90578c6d0ffbc
Shawn Brooks <shawn.brooks@maximintegrated.com>
2016-09-01 12:26:12 -0500

    make ROOT relative to side-step cygwin path issues

--- tools/settings.py
+++ tools/settings.py
@@ -19,7 +19,7 @@ from os import getenv
 from os.path import join, abspath, dirname, exists
 import logging
 
-ROOT = abspath(join(dirname(__file__), ".."))
+ROOT = ".."
 
 
 ##############################################################################
